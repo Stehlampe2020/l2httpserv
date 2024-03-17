469af8d81eb431904efd01bf454a6f95ca482309
Lampe2020 <Stehlampe2020@gmail.com>
2024-03-17 23:46:56 +0100

    Update filemanager.py * Remove output buffering method because it's now in the base class

--- examples/filemanager.py
+++ examples/filemanager.py
@@ -6,6 +6,3 @@ import main
 class HTTPFileManager(main.L2HTTPServ):
     """Web file manager based on L2HTTPServ"""
     __implemented_methods = 'DELETE,GET,HEAD,POST,PUT'
-
-    def __set_header(self, hname:str, hcont:str=''):
-        """Sets a header"""
