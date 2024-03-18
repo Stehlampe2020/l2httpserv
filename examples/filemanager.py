e67dd6ae012f5ea3e4b3c1096d6e59636690c38e
Lampe2020 <Stehlampe2020@gmail.com>
2024-03-18 11:32:33 +0100

    Update filemanager.py * Complete code skeleton

--- examples/filemanager.py
+++ examples/filemanager.py
@@ -6,3 +6,29 @@ import main
 class HTTPFileManager(main.L2HTTPServ):
     """Web file manager based on L2HTTPServ"""
     __implemented_methods = 'DELETE,GET,HEAD,POST,PUT'
+
+    def __handle_DELETE(self):
+        """Handle DELETE requests"""
+
+    def __handle_GET(self):
+        """Handle GET requests"""
+
+    def __handle_HEAD(self):
+        """Handle HEAD requests"""
+
+    def __handle_POST(self):
+        """Handle POST requests"""
+
+    def __handle_PUT(self):
+        """Handle PUT requests"""
+
+if __name__ == '__main__':
+    """Do this if the module is called as a script."""
+    import argparse
+    argparser:argparse.ArgumentParser = argparse.ArgumentParser(
+        prog='HTTPFileManager',
+        description='A simple file manager rendered in a browser, written in Python3.',
+        epilog='This thing is NOT IN ANY WAY secure, so don\'t expose it to the internet if you want your files to be safe!'
+    )
+    argparser.add_argument('-s', '--ssl', action='store_true', help="Encrypt the connections with SSL ('certfile.crt' and 'keyfile.key' must be present in working dir)")
+    init_server(L2HTTPServ, enable_ssl=(argparser.parse_args().ssl))
